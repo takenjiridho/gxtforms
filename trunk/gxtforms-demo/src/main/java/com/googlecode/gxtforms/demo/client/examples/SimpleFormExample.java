38486bc379900909ae76c020411ded1e4e6600cd
david.durham.jr <david.durham.jr@0c1028da-69ba-11de-859f-df06e5afd5d7>
2009-10-28 17:47:49 +0000

    initial form binding work

--- trunk/gxtforms-demo/src/main/java/com/googlecode/gxtforms/demo/client/examples/SimpleFormExample.java
+++ trunk/gxtforms-demo/src/main/java/com/googlecode/gxtforms/demo/client/examples/SimpleFormExample.java
@@ -1,7 +1,12 @@
 package com.googlecode.gxtforms.demo.client.examples;
 
+import com.extjs.gxt.ui.client.event.ButtonEvent;
+import com.extjs.gxt.ui.client.event.SelectionListener;
 import com.extjs.gxt.ui.client.widget.LayoutContainer;
+import com.extjs.gxt.ui.client.widget.MessageBox;
 import com.extjs.gxt.ui.client.widget.VerticalPanel;
+import com.extjs.gxt.ui.client.widget.button.Button;
+import com.extjs.gxt.ui.client.widget.form.FormPanel;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.rpc.AsyncCallback;
@@ -28,14 +33,28 @@ public class SimpleFormExample extends LayoutContainer {
         FormServiceAsync service = GWT.create(FormService.class);
         service.getFormConfiguration(SimpleForm.class.getName(), new AsyncCallback<FormConfiguration>() {
             public void onSuccess(FormConfiguration result) {
-                vp.add(new GXTFormBuilder().buildFormPanel(result));
-                vp.layout();
+            	addForm(result);
             }
 
             public void onFailure(Throwable caught) {
-
+            	MessageBox.alert("Error", caught.getMessage(), null);
             }
         });
+    }
+    
+    public void addForm(FormConfiguration config) {
+    	final SimpleForm form = new SimpleForm();
+        FormPanel panel = new GXTFormBuilder().buildFormPanel(config, form);
+        Button submit = new Button("Test DataBinding", new SelectionListener<ButtonEvent>() {
+			
+			@Override
+			public void componentSelected(ButtonEvent ce) {
+				MessageBox.alert("Name", form.getName(), null);
+			}
+		});
+		panel.addButton(submit);
+		vp.add(panel);
+		vp.layout();
     }
 
 }
